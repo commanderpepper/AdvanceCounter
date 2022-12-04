a5dd7f9c80bdfa2b410f9194b42cbc877242b7d4
humza <ahmadhumza7@gmail.com>
2022-12-03 17:35:46 -0800

    Removed Composables from AdvanceCounterActivity.kt

--- app/src/main/java/com/commanderpepper/advancecounter/AdvanceCounterActivity.kt
+++ app/src/main/java/com/commanderpepper/advancecounter/AdvanceCounterActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.commanderpepper.advancecounter.ui.parentcounters.ParentCounters
 import com.commanderpepper.advancecounter.ui.theme.AdvanceCounterTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,18 +26,5 @@ class AdvanceCounterActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AdvanceCounterTheme {
-        Greeting("Android")
     }
 }
