3cfc9bd64fc225301c00c1db31604b42c328ee42
humza <ahmadhumza7@gmail.com>
2022-12-04 15:57:05 -0800

    Modified CounterItem to evenly space the plus and minus buttons.

--- ui/src/main/java/com/commanderpepper/advancecounter/ui/items/CounterItemUIState.kt
+++ ui/src/main/java/com/commanderpepper/advancecounter/ui/items/CounterItemUIState.kt
@@ -1,13 +1,12 @@
 package com.commanderpepper.advancecounter.ui.items
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 data class CounterItemUIState(
     val id: Long,
@@ -22,14 +21,14 @@ fun CounterItem(
     onMinusClicked: (Long) -> Unit,
     onPlusClicked: (Long) -> Unit,
 ) {
-    Column(modifier = modifier) {
+    Column(modifier = modifier.padding(8.dp)) {
         Text(text = "Name: ${counterItemUIState.name}, ID: ${counterItemUIState.id}")
         Text(text = "Value: ${counterItemUIState.value}")
-        Row() {
-            Button(onClick = { onMinusClicked(counterItemUIState.id) }) {
+        Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+            Button(modifier = Modifier.weight(1f), onClick = { onMinusClicked(counterItemUIState.id) }) {
                 Text(text = "-")
             }
-            Button(onClick = { onPlusClicked(counterItemUIState.id) }) {
+            Button(modifier = Modifier.weight(1f), onClick = { onPlusClicked(counterItemUIState.id) }) {
                 Text(text = "+")
             }
         }
