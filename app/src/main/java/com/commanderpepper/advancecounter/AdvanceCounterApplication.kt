93961dffdbef1f518851f5a100c91ffd83dfc685
humza <ahmadhumza7@gmail.com>
2022-12-03 16:25:59 -0800

    Added more Hilt related dependencies. Added a custom Application class named AdvanceCounterApplication.kt.

---
+++ app/src/main/java/com/commanderpepper/advancecounter/AdvanceCounterApplication.kt
@@ -0,0 +1,8 @@
+package com.commanderpepper.advancecounter
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AdvanceCounterApplication: Application() {
+}
