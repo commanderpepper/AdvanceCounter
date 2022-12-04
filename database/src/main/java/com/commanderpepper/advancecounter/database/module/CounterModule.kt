2a37215ca879de48d5609a340415fc4b9f122b28
humza <ahmadhumza7@gmail.com>
2022-12-03 16:38:35 -0800

    Added CounterModule.kt

---
+++ database/src/main/java/com/commanderpepper/advancecounter/database/module/CounterModule.kt
@@ -0,0 +1,29 @@
+package com.commanderpepper.advancecounter.database.module
+
+import android.content.Context
+import androidx.room.Room
+import com.commanderpepper.advancecounter.database.room.CounterDatabase
+import com.commanderpepper.advancecounter.database.room.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface CounterModule {
+
+    @Singleton
+    @Provides
+    fun provideCounterDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(
+        context,
+        CounterDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun providesCounterDAO(counterDatabase: CounterDatabase) = counterDatabase.counterDao()
+}
