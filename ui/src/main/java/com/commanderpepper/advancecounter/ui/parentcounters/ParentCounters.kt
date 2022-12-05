28bafa392593841daa85e0a77ce94d275c390e3f
humza <ahmadhumza7@gmail.com>
2022-12-05 13:58:04 -0800

    Modified the UI to move the add new counter button to the bottom right.

--- ui/src/main/java/com/commanderpepper/advancecounter/ui/parentcounters/ParentCounters.kt
+++ ui/src/main/java/com/commanderpepper/advancecounter/ui/parentcounters/ParentCounters.kt
@@ -1,6 +1,8 @@
 package com.commanderpepper.advancecounter.ui.parentcounters
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -9,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.commanderpepper.advancecounter.ui.AddCounterDialog
 import com.commanderpepper.advancecounter.ui.items.CounterItem
@@ -21,18 +25,22 @@ fun ParentCounters(
     modifier: Modifier = Modifier,
     parentCountersViewModel: ParentCountersViewModel = hiltViewModel()
 ) {
-    Column {
-        val openDialog = remember { mutableStateOf(false) }
 
-        Button(onClick = { openDialog.value = true }) {
-            Text(text = "Add new counter")
-        }
+    Box(modifier = Modifier.fillMaxSize()) {
+        val openDialog = remember { mutableStateOf(false) }
         ParentCounters(
             modifier = modifier,
             parentCounters = parentCountersViewModel.parentCounters,
             onPlusClicked = parentCountersViewModel::plusButtonOnClick,
             onMinusClicked = parentCountersViewModel::minusButtonOnClick
         )
+        Button(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp),
+            onClick = { openDialog.value = true }) {
+            Text(text = "Add new counter")
+        }
         if (openDialog.value) {
             AddCounterDialog(
                 onDismissRequest = { openDialog.value = false },
