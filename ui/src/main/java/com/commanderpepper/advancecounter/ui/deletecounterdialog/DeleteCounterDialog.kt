a8895d56c1820918d238a4e5c38cbfd07705f564
humza <ahmadhumza7@gmail.com>
2022-12-09 13:21:57 -0800

    Added a dialog for deleting a counter.

---
+++ ui/src/main/java/com/commanderpepper/advancecounter/ui/deletecounterdialog/DeleteCounterDialog.kt
@@ -0,0 +1,35 @@
+package com.commanderpepper.advancecounter.ui.deletecounterdialog
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DeleteCounterDialog(
+    modifier: Modifier,
+    onConfirmClick: () -> Unit,
+    onDismissRequest: () -> Unit
+){
+    AlertDialog(
+        modifier = modifier,
+        title = {
+            Text("Delete Counter")
+        },
+        text = {
+               Text(text = "Do you want to delete the counter?")
+        },
+        dismissButton = {
+            Button(onClick = { onDismissRequest() }) {
+                Text(text = "Do not delete")
+            }
+        },
+        confirmButton = {
+            Button(onClick = { onConfirmClick() }) {
+                Text(text = "Delete Counter")
+            }
+        },
+        onDismissRequest = onDismissRequest
+    )
+}
