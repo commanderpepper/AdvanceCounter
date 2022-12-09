754f9583622449e52ae314e8733d7f261dd3de9e
humza <ahmadhumza7@gmail.com>
2022-12-09 12:46:55 -0800

    Moved counter item options UI to its own file.

---
+++ ui/src/main/java/com/commanderpepper/advancecounter/ui/items/CounterItemOptionsButton.kt
@@ -0,0 +1,40 @@
+package com.commanderpepper.advancecounter.ui.items
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.res.painterResource
+
+@Composable
+fun CounterItemOptionsButton(
+                         counterOptionImageResource: Int,
+                         onEditClicked: () -> Unit,
+                         onDeleteClicked: () -> Unit) {
+    val isDropdownMenuVisible = remember { mutableStateOf(false) }
+    IconButton(onClick = {
+        isDropdownMenuVisible.value = true
+    }) {
+        val painter = painterResource(id = counterOptionImageResource)
+        Icon(painter = painter, contentDescription = "Add new counter")
+        DropdownMenu(expanded = isDropdownMenuVisible.value, onDismissRequest = {
+            isDropdownMenuVisible.value = false
+        }) {
+            DropdownMenuItem(
+                text = { Text(text = "Edit Counter") },
+                onClick = {
+                    onEditClicked()
+                    isDropdownMenuVisible.value = false
+                }
+            )
+            DropdownMenuItem(
+                text = { Text(text = "Delete Counter") },
+                onClick = {
+                    // TODO: Replace with method from view model
+                    onDeleteClicked()
+                    isDropdownMenuVisible.value = false
+                }
+            )
+        }
+    }
+}
